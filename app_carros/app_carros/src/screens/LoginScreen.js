21c442013f7075375fbc6c50696f182660de2af3
Vectt0r <douglas_sd0@outlook.com>
2024-01-25 14:13:47 -0300

    net_app

--- app_carros/app_carros/src/screens/LoginScreen.js
+++ app_carros/app_carros/src/screens/LoginScreen.js
@@ -1,14 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, TouchableOpacity, Image, StyleSheet, StatusBar } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import NetInfo from '@react-native-community/netinfo';
 
 const LoginScreen = ({ navigation }) => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [isConnected, setIsConnected] = useState(true);
 
   const apiUrl = 'http://172.20.15.149:3000/api/login';
 
+  //Teste a conexão do usuario
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener((state) => {
+      setIsConnected(state.isConnected);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  //carregar o último nome de usuário
+  useEffect(() => {
+    const loadLastUsername = async () => {
+      try {
+        // Recuperar o último nome de usuário do AsyncStorage
+        const lastUsername = await AsyncStorage.getItem('lastUsername');
+        if (lastUsername) {
+          setUsername(lastUsername);
+        }
+      } catch (error) {
+        console.error('Erro ao carregar o último nome de usuário:', error);
+      }
+    };
+
+    loadLastUsername();
+  }, []);
+
+  //Usuario conectado
   const handleLogin = async () => {
+    if (!isConnected) {
+      alert('Sem conexão com a internet. Por favor, verifique sua conexão e tente novamente.');
+      return;
+    }
     try {
       const response = await fetch(apiUrl, {
         method: 'POST',
@@ -24,15 +59,17 @@ const LoginScreen = ({ navigation }) => {
       const data = await response.json();
 
       if (response.ok) {
-        // Certifique-se de que o token não é undefined antes de armazená-lo
+        //Manter ultimo usuario no campo de login
+        await AsyncStorage.setItem('lastUsername', username);
+
         if (data.token) {
-          await AsyncStorage.setItem('accessToken', data.token); // Modifique conforme necessário
+          await AsyncStorage.setItem('accessTokenNetCometApp', data.token); // Token de acesso do usario
           navigation.navigate('Home');
         } else {
           console.error('Token de acesso indefinido');
         }
       } else {
-        // Exiba uma mensagem de erro ou alerta caso a autenticação falhe
+        //alerta caso a autenticação falhe
         alert('Credenciais inválidas');
       }
     } catch (error) {
